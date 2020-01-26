b804b48bcf011a63e9725a36d1ace573911ce0c2
Tomas <tomas.huzevka@outlook.com>
2020-01-26 14:30:16 -0500

    1/26/2020

--- server.py
+++ server.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
     print(request.form)
     return render_template("submit.html", user_name=request.form['name'], location=request.form['dojo_location'], language=request.form['favorite_language'], com=request.form['comment'])
 
-
-
-    
 
 if __name__ == "__main__":
     app.run(debug=True)
